8296e72c3094ec3404e23b31792fbab8245431fc
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-06-06 00:33:16 +0700

    new

---
+++ components/NavbarCompounent.js
@@ -0,0 +1,32 @@
+
+
+const NavbarCompounent = () => {
+  return (
+   <>
+        <div className="Navbar">
+            <div className="headingText"> 
+                <h1>TechShed</h1>
+            </div>
+
+            <div className="Menu-Icon">
+                <ul className="Icon">
+                    <li>icon1</li>
+                    <li>icon2</li>
+                    <li>icon3</li>
+                    <li>icon4</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div className="Menu-Bar">
+            <li>Shop All</li>
+            <li>Computers</li>
+            <li>Tablets</li>
+            <li>Drones & Cameras</li>
+        </div>
+   </>
+  )
+}
+
+export default NavbarCompounent
---
+++ src/app/Homepage/Homepage.js
@@ -0,0 +1,71 @@
+
+export default function Home() {
+  return (
+  <>
+    <div className="HederSectionHome">
+
+    
+       <div className="HeroContent">
+          <h1>Incredible Prices on All Your Favorite Items</h1>
+          <p>Get more for less on selected brands</p>
+          <br/>
+          <button className="btn-buy">ShopNow</button>
+        </div>
+
+
+    </div>
+
+
+
+    <div className="DealSection">
+      <div className="HolidayDeals">
+          <div className="HeroHoliday">
+            <p>Holiday Deals</p>
+            <br/>
+            <h1>Up to 30% off</h1>
+            <br/>
+            <p>Selected Smartphone Brands</p>
+            <br/>
+            <br/>
+            <button className="btn-shop">Shop</button>
+          </div>
+      </div>
+
+        <div className="Just-In">
+          <div className="HeroJustIn">
+            <p>Just In</p>
+            <br/>
+            <h1>Take Your Sound Anywhere</h1>
+            <br/>
+            <p>Top Headphone Brands</p>
+            <br/>
+            <br/>
+            <button className="btn-shop">Shop</button>
+          </div>
+      </div>
+      
+    </div>
+     <div className="Front-Deliverly-Wrapper">
+      <div className="Front-Deliverly">
+          <div className="Deliverly-1">
+            <b><p>Curb-Side pickup</p></b>
+          </div>
+          <div className="Deliverly-2">
+            <b><p>Free Shipping on Orders Over $50</p></b>
+          </div>
+          <div className="Deliverly-3">
+            <b><p>Low prices guaranteed</p></b>
+          </div>
+          <div className="Deliverly-4">
+            <b><p>Available to you 24/7</p></b>
+          </div>
+      </div>
+    </div>
+
+
+   
+    
+      
+  </>
+  );
+} 
